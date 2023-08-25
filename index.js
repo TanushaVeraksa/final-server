aeebf05d71ca842f8197c7a3fd8c1fe1310b6bd4
TanushaVeraksa <veraksa161@gmail.com>
2023-08-25 21:41:50 +0300

    change index

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const config = require('config');
+//const config = require('config');
 
 const app = express();
 
-const port = config.get('port') || 5000;
+//const port = config.get('port') || 5000;
+const port = 5000;
 
 app.get('/', (req, res) => res.send('Home Page Route'));
 
 const start = async() => {
     try{
-        await mongoose.connect(config.get('dbUrl'));
+        ///await mongoose.connect(config.get('dbUrl'));
+        await mongoose.connect('mongodb+srv://veraksa161:vlu2Otgeq0D7nM2o@cluster0.1lxltk8.mongodb.net/?retryWrites=true&w=majority');
         app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
     } catch(e) {
         console.dir(e)
