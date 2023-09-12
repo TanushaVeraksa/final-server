3248616dbd199debb4813060769bb90e8d95d596
TanushaVeraksa <veraksa161@gmail.com>
2023-09-13 00:43:10 +0300

    added in comment.routes cors

--- index.js
+++ index.js
@@ -24,14 +24,7 @@ app.use('/api/user', authMiddleware, checkRouter);
 app.use('/api/review', jsonParser, reviewRouter);
 app.use('/api/piece', jsonParser, pieceRouter);
 app.use('/api/personal', jsonParser, personalRouter);
-app.use();
-app.use('/api/comment', function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    }, commentRouter);
+app.use('/api/comment', commentRouter);
 
 const start = async() => {
     try{
--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -6,6 +6,12 @@ const events = require('events');
 
 const emitter = new events.EventEmitter();
 
+router.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
+
 // router.get('/get-comment', async(req, res) => {
 //     emitter.once('newMessage', (comment)=> {
 //         res.json(comment)
