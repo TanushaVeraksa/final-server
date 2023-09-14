cd975198a1bc7890ce26cb29b8d7f1cc3f9e954f
TanushaVeraksa <veraksa161@gmail.com>
2023-09-14 12:54:14 +0300

    deleted proxy

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const passport = require('passport');
 const instagramRoutes = require('./routes/instagram.routes');
 const githubRoutes = require('./routes/github.routes');
 const session = require('express-session');
-const proxy = require('http-proxy-middleware');
 
 const app = express();
 
@@ -27,31 +26,6 @@ app.use(
       preflightContinue: true,
     })
   );
-
-  app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.use(proxy('/api', {
-    target: 'https://final-client-livid.vercel.app/',
-    logLevel: 'debug',
-    changeOrigin: true
-}));
 
 app.use(
     session({
