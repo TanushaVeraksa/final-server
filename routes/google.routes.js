78eab9448716a673a0e0154dba23b568adc90964
TanushaVeraksa <veraksa161@gmail.com>
2023-09-24 22:28:43 +0300

    delete parseCookies function

--- routes/google.routes.js
+++ routes/google.routes.js
@@ -46,23 +46,6 @@ passport.authenticate('google', {
 })
 );
 
-function parseCookies (request) {
-  const list = {};
-  const cookieHeader = request.headers?.cookie;
-  if (!cookieHeader) return list;
-
-  cookieHeader.split(`;`).forEach(function(cookie) {
-      let [ name, ...rest] = cookie.split(`=`);
-      name = name?.trim();
-      if (!name) return;
-      const value = rest.join(`=`).trim();
-      if (!value) return;
-      list[name] = decodeURIComponent(value);
-  });
-
-  return list;
-}
-
 router.get('/error', (req, res) => res.send('Error logging in via Google..'));
 router.get('/profile', (req, res) => {
   res.cookie('user', req.session.passport.user.id)
